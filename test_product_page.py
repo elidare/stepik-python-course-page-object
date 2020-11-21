fd8fd80029b15b56565c959c333b17790beb320a
elidare <elidare.jjj@gmail.com>
2020-11-21 19:12:30 +0300

    Add link parameters to product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,16 @@
 from pages.product_page import ProductPage
+import pytest
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+urls = [f"{product_base_link}/?promo=offer{n}" for n in range(10)]
+
+# ?promo=offer7 has expected bug on the page
+urls[7] = pytest.param(urls[7], marks=pytest.mark.xfail)
+
+
+@pytest.mark.parametrize('link', urls)
+def test_guest_can_add_product_to_basket(browser, link):
     product_page = ProductPage(browser, link)
     product_page.open()
     product_data = product_page.get_product_data()
