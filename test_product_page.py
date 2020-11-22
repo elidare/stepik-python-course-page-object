0badfb0a44545c14cd1c4e82548fd0065904d886
elidare <elidare.jjj@gmail.com>
2020-11-22 23:16:04 +0300

    Delete language from product links

--- test_product_page.py
+++ test_product_page.py
@@ -12,14 +12,14 @@ urls[7] = pytest.param(urls[7], marks=pytest.mark.xfail)
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.should_be_login_link()
 
 
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.go_to_login_page()
