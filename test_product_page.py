d18a963f8db8b34bd6df41a75e99b6bf3ac738f1
elidare <elidare.jjj@gmail.com>
2020-11-22 23:35:28 +0300

    Add pytest.mark.login_guest and reorganize product_page tests

--- test_product_page.py
+++ test_product_page.py
@@ -11,20 +11,21 @@ urls = [f"{product_base_link}/?promo=offer{n}" for n in range(10)]
 urls[7] = pytest.param(urls[7], marks=pytest.mark.xfail)
 
 
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.should_be_login_link()
+@pytest.mark.login_guest
+class TestLoginFromProductPage():
+    def test_guest_should_see_login_link_on_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.should_be_login_link()
 
-
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
@@ -36,31 +37,42 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page.should_have_empty_text()
 
 
-class TestAddToBasket():
-    @pytest.mark.parametrize('link', urls)
-    def test_guest_can_add_product_to_basket(self, browser, link):
-        product_page = ProductPage(browser, link)
+class TestUserAddToBasketFromProductPage():
+    def test_user_can_add_product_to_basket(self, browser):
+        product_page = ProductPage(browser, product_base_link)
         product_page.open()
         product_data = product_page.get_product_data()
         product_page.add_product_to_basket()
-        product_page.solve_quiz_and_get_code()
         product_page.should_be_added_to_basket(*product_data)
 
-    @pytest.mark.xfail
-    def test_guest_cant_see_success_message_after_adding_product_to_basket(self, browser):
-        product_page = ProductPage(browser, product_base_link)
-        product_page.open()
-        product_page.add_product_to_basket()
-        product_page.should_not_have_success_message()
-
-    def test_guest_cant_see_success_message(self, browser):
+    def test_user_cant_see_success_message(self, browser):
         product_page = ProductPage(browser, product_base_link)
         product_page.open()
         product_page.should_not_have_success_message()
 
-    @pytest.mark.xfail
-    def test_message_disappeared_after_adding_product_to_basket(self, browser):
-        product_page = ProductPage(browser, product_base_link)
-        product_page.open()
-        product_page.add_product_to_basket()
-        product_page.should_disappear_success_message()
+
+@pytest.mark.parametrize('link', urls)
+@pytest.mark.skip
+def test_guest_can_add_product_to_basket(browser, link):
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_data = product_page.get_product_data()
+    product_page.add_product_to_basket()
+    product_page.solve_quiz_and_get_code()
+    product_page.should_be_added_to_basket(*product_data)
+
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    product_page = ProductPage(browser, product_base_link)
+    product_page.open()
+    product_page.add_product_to_basket()
+    product_page.should_not_have_success_message()
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    product_page = ProductPage(browser, product_base_link)
+    product_page.open()
+    product_page.add_product_to_basket()
+    product_page.should_disappear_success_message()
