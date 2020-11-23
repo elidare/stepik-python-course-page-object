310f3c555f1471ba3345a5536656c854a78f1507
elidare <elidare.jjj@gmail.com>
2020-11-23 23:03:26 +0300

    Delete unnecessary implicit timeout

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,10 +6,9 @@ import math
 
 
 class BasePage:
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-        # self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
